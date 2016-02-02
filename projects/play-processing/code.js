4e844357e5b57c445879baf9a985c519eae4f025
jrdhuntington <Jrdhuntington@gmail.com>
2016-02-02 00:05:04 +0000

    install bouncing box + play processing

---
+++ projects/play-processing/code.js
@@ -0,0 +1,50 @@
+// wooden table
+background(186, 145, 20);
+
+// plate
+ellipse(200, 200, 350, 350);
+ellipse(200, 200, 300, 300); 
+
+// eggs
+fill(255, 255, 255);
+ellipse(170, 105, 116, 84);
+fill(255, 255, 71);
+ellipse(170, 105, 60, 60);
+fill(255, 255, 255);
+ellipse(118, 178, 108, 84);
+fill(255, 255, 71);
+ellipse(119, 178, 60, 60);
+
+// toast
+fill(255, 241, 168);
+rect(128, 244, 143, 116);
+
+// toast2
+arc(168, 245, 89, 75, 155, 360);
+arc(230, 245, 93, 75, 155, 387);
+
+//fill in
+fill(255, 241, 168);
+stroke(255, 241, 168);
+strokeWeight(10);
+point(196, 223);
+fill(255, 241, 168);
+stroke(255, 241, 168);
+strokeWeight(10);
+point(189, 226);
+fill(255, 241, 168);
+stroke(255, 241, 168);
+strokeWeight(10);
+point(185, 234);
+fill(255, 241, 168);
+stroke(255, 241, 168);
+strokeWeight(10);
+point(185, 242);
+fill(255, 241, 168);
+stroke(255, 241, 168);
+strokeWeight(10);
+point(185, 251);
+fill(255, 241, 168);
+stroke(255, 241, 168);
+strokeWeight(10);
+point(188, 258);
